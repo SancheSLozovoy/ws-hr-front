073ba8512f1cd9fd0b24ea843589f650ededf874
SanchesLozovoy <alex.lozovoy05@bk.ru>
2024-10-18 10:07:01 +0300

    feat: added a method to create link uniqueness

--- src/features/getTasks/api/getTaskLinkApi.ts
+++ src/features/getTasks/api/getTaskLinkApi.ts
@@ -1,14 +1,22 @@
 import axios from "axios";
+import { invalidTask } from "../../taskToInvalid/api/invalidTask";
 
 const fetchTaskLink = async (documentId: string) => {
     try {
         const taskLinkResponse = await axios.get(`http://localhost:1337/api/task-links/${documentId}`);
         
         const taskId = taskLinkResponse.data.data.id;
+        const isValid = taskLinkResponse.data.data.isValid;
 
-        const response = await axios.get(`http://localhost:1337/api/tasks?filters[task][$eq]=${taskId}`);
-        
-        return response.data.data
+        if(isValid){
+            const response = await axios.get(`http://localhost:1337/api/tasks?filters[task][$eq]=${taskId}`);
+
+            invalidTask(documentId, false)
+
+            console.log(isValid)
+            return response.data.data
+
+        }
     } catch (err) {
         console.error(err);
     }
